4a6cc8c630a822fb883d64c3b1944a19407b1042
shodmonbek571 <shodmonbekakbarov820@gmail.com>
2024-01-14 14:46:49 +0500

    HOMEWORK

---
+++ index.js
@@ -0,0 +1,85 @@
+//  1-MASALA
+
+// var berilganSon = -7; 
+
+// if (berilganSon > 0) {
+//     console.log("Berilgan son musbat");
+// } else {
+//     console.log("Berilgan son manfiy");
+// }
+
+//  2-MASALA
+
+// var berilganSon = 75;
+// if (berilganSon >= 90 && berilganSon <= 100) {
+//     console.log("A");
+// }  else if (berilganSon >= 80 && berilganSon <= 89) {
+//     console.log("B");
+// } else if (berilganSon >= 70 && berilganSon <= 79) {
+//     console.log("C");
+// }  else if (berilganSon >= 60 && berilganSon <= 69) {
+//     console.log("D");
+// }  else if (berilganSon >= 0 && berilganSon <= 59) {
+//     console.log("F");
+// }
+//   3-MASALA   "Kabisa yili emas"    + "Kabisa yil"
+
+
+// function aniqlash(kabisaYil) {
+//     if ((kabisaYil % 4 === 0 && kabisaYil % 100 !== 0) || kabisaYil % 400 === 0){
+//       return true;
+//     } else {
+//       return false;
+//   }
+// }
+// console.log(aniqlash(2024));
+// 4 - MASALA
+
+// let num = 15;
+
+// if (num > 0 ) {
+//     console.log("musbat son");
+// } else if (num < 0) {
+//     console.log("manfiy son");
+// } else if (num == 0) {
+//     console.log("Nol");
+// }
+//  5 - MASALA
+// let num1 = 20;
+// let num2 = 30;
+
+// if (num1 > num2 ) {
+//    console.log(num1)
+// } else { 
+// console.log(num2);
+// }
+//  6 - MASALA
+// let num1 = 55;
+// let num2 = 17;
+// let num3 = 26;
+// if (num1 < num2 && num1 < num3) {
+//     console.log(num1);
+// } else {
+//     num2 < num1 && num2 < num3;
+//     console.log(num2);
+// }
+// if (num3 < num1 && num3 < num2) {
+//     console.log(num3);
+// }
+// 7 - MASALA
+// let num1 = -55;
+// let num2 = -17;
+// let num3 = 26;
+//     var res = 0;
+//     if (num1 < 0) {
+//         res += num1;
+//         }
+//     if (num2 < 0) {
+//         res += num2;
+//     }
+//     if (num3 < 0) {
+//         res += num3;
+//     }
+   
+// console.log(res);
+
